9c63129dd4d8bd5bef7b1f727baf47f131f3dd60
mushfichowdhury <chowdhurymushfi@gmail.com>
2021-03-08 02:05:12 -0500

    added meta

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,4 +1,6 @@
 import { Flex, useColorMode } from '@chakra-ui/react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import theme from '../theme';
 const { gunmetal, phthalogreen, timberwolf, culturedwhite, mintcream } = theme.colors;
 
@@ -8,7 +10,35 @@ export const Container = (props) => {
   const bgColor = { light: timberwolf, dark: phthalogreen }
   const headingColor = { light: phthalogreen, dark: mintcream }
   const textColor = { light: phthalogreen, dark: culturedwhite }
+
+  const router = useRouter();
+  const meta = {
+    title: 'Mushfi - Software Engineer',
+    description:
+      'Mushfi owns a tortoise.',
+    image: '/',
+    type: 'website'
+  };
+
   return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="meta" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://mushfi.dev${router.asPath}`}
+        />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Mushfi Chowdhury" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
     <Flex
       direction="column"
       alignItems="center"
@@ -17,6 +47,7 @@ export const Container = (props) => {
       headingColor={headingColor[colorMode]}
       textColor={textColor[colorMode]}
       {...props}
-    />
+      />
+      </>
   )
 }
